abeafef72cf049133ba78e54f16280b394d60c11
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-07-08 07:57:57 -0500

    feat: :construction: Avance formulario
    - El formulario extrae correctamente la informacion
    - El programa crea las clases correspondientes e imprime la informacion en consola

--- js/main.js
+++ js/main.js
@@ -9,11 +9,44 @@ class Persona{
         this.#sexo = sexo;
     }
 
+    get getNombre(){
+        return this.#nombre;
+    }
+
+    get esMayorEdad(){
+        if (this.#edad >= 18){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 
     saludar(){
         return `Hola ${this.#nombre}`;
     }
 }
+
+class Estudiante extends Persona {
+    #carrera;
+
+    constructor(nombre, edad, sexo, carrera) {
+        super(nombre, edad, sexo)
+        this.#carrera = carrera;
+    }
+
+    estudiar() {
+        return `${this.getNombre} está estudiando ${this.#carrera}`;
+    }
+}
+
+
+
+
+
+
+
+
 
 
 let btn = document.querySelector("#btn");
@@ -22,7 +55,12 @@ btn.addEventListener("click", function(){
     let nombre = document.querySelector("#name").value;
     let edad = document.querySelector("#age").value;
     let sexo = document.querySelector("#sex").value;
+    let carrera = document.querySelector("#job").value;
 
     let persona1 = new Persona(nombre, edad, sexo);
+    let estudiante1 = new Estudiante(nombre, edad, sexo, carrera);
+
     console.log(persona1.saludar());
+    console.log(estudiante1.estudiar());
+    console.log(`¿Mayor de edad?: ${estudiante1.esMayorEdad}`);
 })
